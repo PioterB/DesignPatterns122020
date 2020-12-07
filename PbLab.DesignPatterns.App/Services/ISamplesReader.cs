e27dfd8318e1f8bfdfc9a0290145d06c89fb74c1
Piotr Barankiewicz <pbarank@ra.rockwell.com>
2020-12-07 01:47:19 +0100

    Add project files.

---
+++ PbLab.DesignPatterns.App/Services/ISamplesReader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using PbLab.DesignPatterns.Model;
+
+namespace PbLab.DesignPatterns.Services
+{
+    internal interface ISamplesReader
+    {
+        IEnumerable<Sample> Read(StreamReader stream);
+    }
+}
